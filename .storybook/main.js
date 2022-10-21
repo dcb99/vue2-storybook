c514ec67e7ec701d3b1a2df6c4d4973ca22b98e0
David <dbarless@gmail.com>
2022-10-21 14:01:44 -0400

    Update storybook config for scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ const loader = require('sass-loader');
 
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-scss'],
     framework: '@storybook/vue',
     core: {
         builder: '@storybook/builder-webpack5',
@@ -13,20 +13,6 @@ module.exports = {
         config.resolve.alias['@'] = path.resolve(__dirname, '..', 'src');
         config.resolve.alias['~storybook'] = path.resolve(__dirname);
 
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                {
-                    loader: 'style-resources-loader',
-                    options: {
-                        patterns: [path.resolve(__dirname, './src/scss/main.scss')],
-                    },
-                },
-            ],
-            include: path.resolve(__dirname, '../'),
-        });
         return config;
     },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import "./../src/scss/main.scss";
 
 Vue.use(Vuetify);
 const vuetify = new Vuetify({

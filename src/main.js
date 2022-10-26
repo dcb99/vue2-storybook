70825772995e0c40bfad345dabf40749ac2ef8cd
David <dbarless@gmail.com>
2022-10-26 09:55:04 -0400

    Update icon fonts config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import './scss/main.scss';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
